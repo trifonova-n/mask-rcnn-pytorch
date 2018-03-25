431041922b77ef5fb5986913d0fe6ada1b4e0086
GeeshangXu <geeshangxu@gmail.com>
2018-03-25 18:41:40 +0800

    Fix utils and related unittest.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,29 +16,29 @@ class TestUtils(unittest.TestCase):
 
         a2 = torch.zeros(3, 4)
         a2[:, :2] = 0
-        a2[:, 2:] = 100
+        a2[:, 2:] = 99
         b2 = torch.zeros(4, 4)
         b2[:, :2] = 0
-        b2[:, 2:] = 50
+        b2[:, 2:] = 49
         c2 = torch.zeros(3, 4)
         c2[:, :] = 0.25
 
         a3 = torch.zeros(3, 4)
         a3[:, :2] = 0
-        a3[:, 2:] = 100
+        a3[:, 2:] = 99
         b3 = torch.zeros(4, 4)
         b3[:, :2] = 0
-        b3[:, 2:] = 200
+        b3[:, 2:] = 199
         c3 = torch.zeros(3, 4)
         c3[:, :] = 0.25
 
         a4 = torch.zeros(3, 4)
         a4[:, :2] = 50
-        a4[:, 2:] = 150
+        a4[:, 2:] = 149
         b4 = torch.zeros(4, 4)
         b4[:, 0] = 100
         b4[:, 1] = 0
-        b4[:, 2:] = 200
+        b4[:, 2:] = 199
         c4 = torch.zeros(3, 4)
         c4[:, :] = 0.2
 
@@ -47,10 +47,10 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(calc_iou(a3, b3).equal(c3))
         self.assertTrue(calc_iou(a4, b4).equal(c4))
 
-    def test_coord_center2corner(self):
+    def test_coord_corner2center(self):
         a1 = torch.zeros(3, 4)
         a1[:, :2] = 0
-        a1[:, 2:] = 100
+        a1[:, 2:] = 99
         b1 = torch.zeros(3, 4)
         b1[:, :2] = 50
         b1[:, 2:] = 100
@@ -58,8 +58,30 @@ class TestUtils(unittest.TestCase):
         a2 = torch.zeros(3, 4)
         a2[:, 0] = 50
         a2[:, 1] = 100
-        a2[:, 2] = 100
-        a2[:, 3] = 200
+        a2[:, 2] = 99
+        a2[:, 3] = 199
+        b2 = torch.zeros(3, 4)
+        b2[:, 0] = 75
+        b2[:, 1] = 150
+        b2[:, 2] = 50
+        b2[:, 3] = 100
+
+        self.assertTrue(coord_corner2center(a1).equal(b1))
+        self.assertTrue(coord_corner2center(a2).equal(b2))
+
+    def test_coord_center2corner(self):
+        a1 = torch.zeros(3, 4)
+        a1[:, :2] = 0
+        a1[:, 2:] = 99
+        b1 = torch.zeros(3, 4)
+        b1[:, :2] = 50
+        b1[:, 2:] = 100
+
+        a2 = torch.zeros(3, 4)
+        a2[:, 0] = 50
+        a2[:, 1] = 100
+        a2[:, 2] = 99
+        a2[:, 3] = 199
         b2 = torch.zeros(3, 4)
         b2[:, 0] = 75
         b2[:, 1] = 150
--- tools/utils.py
+++ tools/utils.py
@@ -16,16 +16,16 @@ def calc_iou(bbox_a, bbox_b):
     a = bbox_a.size(0)
     b = bbox_b.size(0)
 
-    area_a = (bbox_a[:, 2] - bbox_a[:, 0]) * (bbox_a[:, 3] - bbox_a[:, 1])
+    area_a = (bbox_a[:, 2] - bbox_a[:, 0] + 1) * (bbox_a[:, 3] - bbox_a[:, 1] + 1)
     area_a = area_a.unsqueeze(1).expand(a, b)  # [a, b]
-    area_b = (bbox_b[:, 2] - bbox_b[:, 0]) * (bbox_b[:, 3] - bbox_b[:, 1])
+    area_b = (bbox_b[:, 2] - bbox_b[:, 0] + 1) * (bbox_b[:, 3] - bbox_b[:, 1] + 1)
     area_b = area_b.unsqueeze(0).expand(a, b)  # [a, b]
 
     bbox_a = bbox_a.unsqueeze(1).expand(a, b, 4)  # [a, b, 4]
     bbox_b = bbox_b.unsqueeze(0).expand(a, b, 4)  # [a, b, 4]
     inter_x1_y1 = torch.max(bbox_a[:, :, :2], bbox_b[:, :, :2])
     inter_x2_y2 = torch.min(bbox_a[:, :, 2:], bbox_b[:, :, 2:])
-    inter_w_h = torch.clamp(inter_x2_y2 - inter_x1_y1, min=0)
+    inter_w_h = torch.clamp(inter_x2_y2 - inter_x1_y1 + 1, min=0)
 
     intersection = inter_w_h[:, :, 0] * inter_w_h[:, :, 1]  # [a, b] width * height
     iou = intersection / (area_a + area_b - intersection)
@@ -46,11 +46,11 @@ def coord_corner2center(bbox):
         
     """
     x1, y1, x2, y2 = bbox[:, 0], bbox[:, 1], bbox[:, 2], bbox[:, 3]
-    x = torch.floor((x2 - x1) / 2) + x1
-    y = torch.floor((y2 - y1) / 2) + y1
+    x = torch.floor((x2 - x1 + 1) / 2) + x1
+    y = torch.floor((y2 - y1 + 1) / 2) + y1
 
-    w = x2 - x1
-    h = y2 - y1
+    w = x2 - x1 + 1
+    h = y2 - y1 + 1
 
     x.unsqueeze_(1), y.unsqueeze_(1), w.unsqueeze_(1), h.unsqueeze_(1)
     bbox_trans = torch.cat([x, y, w, h], dim=1)
@@ -72,8 +72,8 @@ def coord_center2corner(bbox):
     x, y, w, h = bbox[:, 0], bbox[:, 1], bbox[:, 2], bbox[:, 3]
     x1 = x - torch.floor(w / 2)
     y1 = y - torch.floor(h / 2)
-    x2 = x + torch.floor(w / 2)
-    y2 = y + torch.floor(h / 2)
+    x2 = x + torch.floor(w / 2) - 1
+    y2 = y + torch.floor(h / 2) - 1
 
     x1.unsqueeze_(1), y1.unsqueeze_(1), x2.unsqueeze_(1), y2.unsqueeze_(1)
 
