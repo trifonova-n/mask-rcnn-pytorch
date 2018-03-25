ed16afe5fe00daa9194ec8c59e2aa8675fd31687
GeeshangXu <geeshangxu@gmail.com>
2018-03-25 14:12:53 +0800

    Add backbone resnet 101 feature extractor.

---
+++ backbone/resnet_101.py
@@ -0,0 +1,24 @@
+from torchvision.models.resnet import resnet101
+import torch.nn as nn
+
+
+class ResNet_101(nn.Module):
+    """ResNet 101 backbone to extract features, using c5.
+    
+    """
+
+    def __init__(self):
+        super(ResNet_101, self).__init__()
+        self.resnet = resnet101(pretrained=True)
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+
+        x = self.resnet.layer1(x)
+        x = self.resnet.layer2(x)
+        x = self.resnet.layer3(x)
+        c5 = self.resnet.layer4(x)
+        return c5
